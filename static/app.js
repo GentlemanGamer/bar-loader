b8fd4a9c44d580dd121a036f87d20da1deed13e8
Michael Cruz <mike@ubercruzer.com>
2020-04-16 21:48:23 -0700

    initial commit

---
+++ static/app.js
@@ -0,0 +1,40 @@
+
+
+function round5(x)
+{
+    return (x % 5) >= 2.5 ? parseInt(x / 5) * 5 + 5 : parseInt(x / 5) * 5;
+}
+
+
+function updateWeights(weight) {
+    var kg = weight / 2.2;
+    document.getElementById("loadKG").innerHTML = Math.round(kg * 10) / 10 + " kg";
+    document.getElementById("loadLBS").innerHTML = weight + " lbs";  
+}
+
+
+
+function changeImage()
+{
+    // var howmany = $("input[name='amt']").val();
+    var weight = document.getElementById("weight").value;
+
+    weight = round5(weight);
+
+    if (weight > 855) {
+        weight = 855;
+    }
+
+    if (weight < 45) {
+        weight = 45;
+    }
+
+    $("#barbell").attr("src","static/img/" + weight + ".png");
+
+    updateWeights(weight);
+
+}
+
+function tryMe(arg) {
+    document.write(arg);
+}
